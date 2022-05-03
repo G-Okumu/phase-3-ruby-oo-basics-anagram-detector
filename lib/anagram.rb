bb4cb3e232df38ee48fec0e1a727026c82619d33
George-Okumu <gokumu12@gmail.com>
2022-05-04 02:56:54 +0300

    assignment complete

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+    end
+
+    def match(word_array)
+        word_array.select {|word| word.chars.sort == @name.chars.sort}
+    end
+end
